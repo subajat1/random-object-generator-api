d7e2471c0f6895551cfcf68278be0d0e2d24f544
subajat1 <bayumunajat@outlook.com>
2022-02-06 18:15:24 +0700

    add url_link to FileGenerationResponse

--- app/api/schemas/file.py
+++ app/api/schemas/file.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class FileGenerationResponse(BaseModel):
     filename: str
     filesize: int
+    url_link: str
 
     class Config:
         anystr_strip_whitespace: True
