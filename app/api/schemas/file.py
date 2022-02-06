c9dd530cbbf359f867e320043d014c5e8bd58afb
subajat1 <bayumunajat@outlook.com>
2022-02-06 18:21:17 +0700

    add response schema for file with links

--- app/api/schemas/file.py
+++ app/api/schemas/file.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class FileGenerationResponse(BaseModel):
     filename: str
     filesize: int
     url_link: str
+
+    class Config:
+        anystr_strip_whitespace: True
+
+
+class FileLinksResponse(BaseModel):
+    id: int
+    filename: str
+    created: datetime
+    url_link: str
+    url_report: str
 
     class Config:
         anystr_strip_whitespace: True
