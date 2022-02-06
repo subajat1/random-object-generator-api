92f0e25da7110d552cf5e774b60fd354ece5f9be
subajat1 <bayumunajat@outlook.com>
2022-02-06 09:54:25 +0700

    add cli helper for db init

--- manage.py
+++ manage.py
@@ -1,8 +1,25 @@
 from flask.cli import FlaskGroup
 
-from app import app
+from app import app, db
+from app.api.models.file import File
 
 cli = FlaskGroup(app)
+
+
+@cli.command('recreate_db')
+def recreate_db() -> None:
+    """CLI helper to recreate db"""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command('seed_db')
+def seed_db() -> None:
+    """CLI helper to seed db with some data"""
+    db.session.add(File(filename='seeded_file_data'))
+    db.session.commit()
+
 
 if __name__ == "__main__":
     cli()
