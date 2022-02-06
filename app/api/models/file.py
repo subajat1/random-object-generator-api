d0b5329bc624baba69d5d7c98b7d201616da6765
subajat1 <bayumunajat@outlook.com>
2022-02-06 10:05:13 +0700

    update filename data schema value from env var

--- app/api/models/file.py
+++ app/api/models/file.py
@@ -1,12 +1,15 @@
+import os
 from datetime import datetime
 from app import db
+
+FILENAME_SIZE = int(os.getenv('g_FILENAME_SIZE', 16))
 
 
 class File(db.Model):
     __tablename__ = "files"
 
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(16), unique=True, nullable=False)
+    filename = db.Column(db.String(FILENAME_SIZE), unique=True, nullable=False)
     created = db.Column(db.DateTime, default=datetime.now())
 
     def __init__(self, filename: str):
