73cbd2a04e66b861177821d03cb1f321cca8579c
subajat1 <bayumunajat@outlook.com>
2022-02-06 09:53:55 +0700

    setup sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('app.config.Config')
+db = SQLAlchemy(app)
 
 
 @app.route("/")
